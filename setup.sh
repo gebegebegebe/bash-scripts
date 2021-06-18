66b0944cd5869b4d89889771ea88a2e125af28e4
gebegebegebe <72191858+gebegebegebe@users.noreply.github.com>
2021-06-18 15:19:55 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 
-links=("discord.com/login" "web.whatsapp.com")
+sites=("discord.com/login" "web.whatsapp.com")
 
 firefoxSearch(){
+  links=("$@")
   for link in "${links[@]}"
   do
-      firefox $link
+      firefox "$link"
   done
 }
 
 if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
-  firefoxSearch links 
+  firefoxSearch "${sites[@]}" 
 else
   echo "Device is not connected to the internet"
 fi
